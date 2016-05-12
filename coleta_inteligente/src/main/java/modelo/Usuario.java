6a4f1725f6e1f9b474b1f16df3293a3c2496bd1f
ersouza <ersouza@ATTBHZW808.att.com.br>
2016-05-12 13:47:19 -0300

    Mudando projeto para usar o Maven.

---
+++ coleta_inteligente/src/main/java/modelo/Usuario.java
@@ -0,0 +1,29 @@
+package modelo;
+
+public class Usuario {
+	
+	private long id;
+	private String nomeUsuario;
+	private String senha;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+
+}
