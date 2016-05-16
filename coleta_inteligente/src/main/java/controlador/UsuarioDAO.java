685ada03f41fafbdbe05084797fe93bd157eecf8
eders <eders@DESKTOP-F7GH8EH>
2016-05-15 21:30:45 -0300

    Implementando testes unitarios para a classe Usuario.

--- coleta_inteligente/src/main/java/controlador/UsuarioDAO.java
+++ coleta_inteligente/src/main/java/controlador/UsuarioDAO.java
@@ -1,5 +1,7 @@
 package controlador;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.NoResultException;
@@ -14,9 +16,7 @@ public class UsuarioDAO {
 	private EntityManager em = factory.createEntityManager();
 	
 	public Usuario getUsuario(String nomeUsuario, String senha){
-		
 		Usuario usuario;
-		
 		try{
 			
 			Query query = em.createQuery("Select u from Usuario u where u.nomeUsuario =:nome and u.senha =:senha");
@@ -24,10 +24,60 @@ public class UsuarioDAO {
 			query.setParameter("senha", senha);
 			usuario = (Usuario) query.getSingleResult();
 		}catch(NoResultException ne){
+			System.out.println(ne.getMessage());
 			usuario = null;
 		}
-			
 		return usuario;
 	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Usuario> findAll(){
+		
+		List<Usuario> usuarios;
+		try{
+			Query query = em.createQuery("Select u from Usuario u");
+			usuarios = query.getResultList();
+		}catch(NoResultException ne){
+			System.out.println(ne.getMessage());
+			usuarios = null;
+		}
+		
+		return usuarios;
+	}
+	
+	public boolean deleteUsuario(Usuario usuario){
+		
+		boolean removido;
+		
+		try{
+			Usuario usuarioRemovido = em.merge(usuario);
+			em.remove(usuarioRemovido);
+			removido = true;
+		}catch(NoResultException ne){
+			System.out.println(ne.getMessage());
+			removido = false;
+		}
+		return removido;
+	}
+
+	public boolean createUsuario(Usuario usuario) {
+		
+		boolean inserido;
+		try {
+			em.getTransaction().begin();
+			em.persist(usuario);
+			em.getTransaction().commit();
+			inserido = true;
+		} catch (NoResultException ne) {
+			System.out.println(ne.getMessage());
+			em.getTransaction().rollback();
+			inserido = false;
+		}
+		
+		return inserido;
+		
+	}
+	
+	
 
 }
--- coleta_inteligente/src/test/java/controlador/TestaUsuarioDAO.java
+++ coleta_inteligente/src/test/java/controlador/TestaUsuarioDAO.java
@@ -1,19 +1,66 @@
 package controlador;
 
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import modelo.Usuario;
 
 public class TestaUsuarioDAO {
 	
+	private UsuarioDAO usuarioDAO = new UsuarioDAO();
+	private Usuario usuario = new Usuario();
+	
+	@Before
+	public void criaUsuario(){
+		usuarioDAO = new UsuarioDAO();
+		usuario = new Usuario();
+	}
+	
 	@Test
-	public void testaUsuarioDAO(){
-		UsuarioDAO user = new UsuarioDAO();
-		Usuario usuario = new Usuario();
+	public void testaGetUsuario(){
 		
-		usuario = user.getUsuario("ederson", "123");
+		usuario = usuarioDAO.getUsuario("ederson", "123");
 		
 		System.out.println(usuario.getNomeUsuario());
 	}
-
+	
+	public void testaRemoveUsuario(){
+		
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		Usuario usuario = new Usuario();
+		
+		usuario = usuarioDAO.getUsuario("ederson", "123");
+		usuarioDAO.deleteUsuario(usuario);
+		
+		usuario = usuarioDAO.getUsuario("ederson", "123");
+		
+	}
+	
+	@Test
+	public void testaInsereUsuario(){
+		
+		usuario.setNomeUsuario("Teste");
+		usuario.setSenha("testando");
+		usuario.setUltimoAcesso(new Date());
+		
+		
+		assertEquals(false, usuarioDAO.createUsuario(usuario));
+		
+	}
+	
+	@Test
+	public void testaFindAll(){
+		
+		List<Usuario> usuarios = usuarioDAO.findAll();
+		
+		for (Usuario usuario : usuarios) {
+			System.out.println(usuario.getNomeUsuario());
+		}
+		
+	}
 }
